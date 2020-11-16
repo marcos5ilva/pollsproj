4e3f95aa56f0130ab272c7ac1e1bdd1724230dae
Marcos Silva <marcos.silva79@gmail.com>
2020-11-16 18:26:47 -0500

    feat:add AuditingConfig, DateAudit, Role, RoleName,UserRepository

---
+++ src/main/java/com/hopefulproj/polls/model/RoleName.java
@@ -0,0 +1,6 @@
+package com.hopefulproj.polls.model;
+
+public enum RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/hopefulproj/polls/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.hopefulproj.polls.repository;
+
+public class UserRepository {
+}
